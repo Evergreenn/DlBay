285c0165f7050ffeb878a18f86b71921b2d642ce
TheGreatPGP <evergreen@protonmail.ch>
2016-08-05 14:53:25 +0200

    add rabbitmq config

---
+++ app/Jobs/UploadFile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DlBay\Jobs;
+
+use DlBay\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UploadFile extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
